165bc0472e904f45a0870bf15509648b3752bec3
Arnaud Zaragoza <zaragoza.arnaud@gmail.com>
2016-02-24 22:52:58 +0100

    Add the JDBC and create the main package

---
+++ Let'sWORK/src/view/Main.java
@@ -0,0 +1,14 @@
+package view;
+
+import controller.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hi on Let'sWORK application !");
+		DataBaseController db = new DataBaseController();
+		// We can use the database here !!
+		db.close();
+	}
+
+}
