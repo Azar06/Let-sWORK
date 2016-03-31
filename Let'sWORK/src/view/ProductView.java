f0d428391483c319baa2fa755857412ad04e8b18
PLFaure <plouis.faure@gmail.com>
2016-03-31 02:27:43 +0200

    productview alleluia!!

--- Let'sWORK/src/view/ProductView.java
+++ Let'sWORK/src/view/ProductView.java
@@ -24,10 +24,18 @@ import java.awt.Toolkit;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 import business.facade.CategoryFacade;
+import business.facade.ProductFacade;
+import business.facade.ServiceFacade;
 import business.facade.UserFacade;
 import business.utils.CategoryReturnState;
+import business.utils.ProductReturnState;
+import business.utils.ServiceReturnState;
 import persistence.Category;
 import persistence.CategorySet;
+import persistence.Product;
+import persistence.ProductSet;
+import persistence.Service;
+import persistence.ServiceSet;
 
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
@@ -40,22 +48,32 @@ import javax.swing.SpinnerListModel;
 
 public class ProductView extends AbstractContentView implements ActionListener {
 
-	private CategoryFacade facade;
-	private JTextField newNameField;
+	private CategoryFacade catFacade;
+	private ProductFacade prFacade;
+	private JTextField newLabelField;
 	private JTextArea newDescrArea;
-	private JTextField updateNameField;
+	private JTextField updateLabelField;
 	private JTextArea updateDescrArea;
-	private JSpinner updateProductnameSpinner;
-	private JSpinner deleteProductnameSpinner;
+	private JSpinner updateProductlabelSpinner;
+	private JSpinner deleteProductlabelSpinner;
+	private JSpinner categorySpinner;
+	private JSpinner updateServicecategorySpinner;
 	private CategorySet categories;
-	private Category selectedCategory;
-	private Category selectedCategoryToDelete = null;
+	private Category selectedCategoryForCreation = null;
+	private Category selectedCategory = null;
+	private ProductSet products;
+	private Product selectedProduct;
+	private Product selectedProductToDelete = null;
+	private JTextField newBrandField;
+	private JTextField updateBrandField;
 
 	public ProductView() {
-		this.facade = new CategoryFacade();
+		this.catFacade = new CategoryFacade();
+		this.prFacade = new ProductFacade();
 		setLayout(null);
-		this.categories = this.facade.getCategorySet();
-		this.selectedCategory = null;
+		this.categories = this.catFacade.getCategorySet();
+		this.products = this.prFacade.getProductSet();
+		this.selectedProduct = null;
 
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(55, 25, 525, 506);
@@ -70,23 +88,28 @@ public class ProductView extends AbstractContentView implements ActionListener {
 		lblNewProduct.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		panel.add(lblNewProduct);
 
-		JLabel lblName = new JLabel("Name");
-		lblName.setBounds(20, 44, 39, 19);
-		lblName.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		panel.add(lblName);
+		JLabel lblLabel = new JLabel("Label");
+		lblLabel.setBounds(20, 44, 39, 19);
+		lblLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel.add(lblLabel);
 
-		newNameField = new JTextField();
-		newNameField.setBounds(65, 43, 222, 22);
-		panel.add(newNameField);
-		newNameField.setColumns(10);
-
+		newLabelField = new JTextField();
+		newLabelField.setBounds(65, 43, 188, 22);
+		panel.add(newLabelField);
+		newLabelField.setColumns(10);
+		
+		JLabel lblCategory = new JLabel("Category");
+		lblCategory.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblCategory.setBounds(265, 41, 69, 25);
+		panel.add(lblCategory);
+		
 		JLabel lblDescription = new JLabel("Description");
-		lblDescription.setBounds(20, 89, 78, 19);
+		lblDescription.setBounds(20, 104, 78, 19);
 		lblDescription.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		panel.add(lblDescription);
 
 		newDescrArea = new JTextArea();
-		newDescrArea.setBounds(100, 89, 327, 56);
+		newDescrArea.setBounds(100, 103, 327, 42);
 		panel.add(newDescrArea);
 
 		JSeparator separator = new JSeparator();
@@ -98,20 +121,20 @@ public class ProductView extends AbstractContentView implements ActionListener {
 		lblUpdateAProduct.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		panel.add(lblUpdateAProduct);
 
-		JLabel lblProductName = new JLabel("Product name");
-		lblProductName.setBounds(23, 195, 101, 19);
-		lblProductName.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		panel.add(lblProductName);
+		JLabel lblProductLabel = new JLabel("Product label");
+		lblProductLabel.setBounds(23, 195, 88, 19);
+		lblProductLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel.add(lblProductLabel);
 
 		JLabel lblDeleteAProduct = new JLabel("Delete a product");
 		lblDeleteAProduct.setBounds(10, 393, 159, 25);
 		lblDeleteAProduct.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		panel.add(lblDeleteAProduct);
 
-		JLabel lblDelProductName = new JLabel("Product name");
-		lblDelProductName.setBounds(23, 424, 101, 19);
-		lblDelProductName.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		panel.add(lblDelProductName);
+		JLabel lblDelProductLabel = new JLabel("Product label");
+		lblDelProductLabel.setBounds(23, 424, 88, 19);
+		lblDelProductLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel.add(lblDelProductLabel);
 
 		JButton btnUpdate = new JButton("Update");
 		btnUpdate.setBounds(417, 352, 96, 27);
@@ -133,10 +156,10 @@ public class ProductView extends AbstractContentView implements ActionListener {
 		separator_1.setBounds(10, 385, 503, 2);
 		panel.add(separator_1);
 
-		JLabel lblUpdateName = new JLabel("Name");
-		lblUpdateName.setBounds(54, 233, 33, 16);
-		lblUpdateName.setEnabled(false);
-		panel.add(lblUpdateName);
+		JLabel lblUpdateLabel = new JLabel("Label");
+		lblUpdateLabel.setBounds(54, 236, 33, 16);
+		lblUpdateLabel.setEnabled(false);
+		panel.add(lblUpdateLabel);
 
 		JButton btnAdd = new JButton("Add");
 		btnAdd.setBounds(433, 118, 80, 27);
@@ -145,61 +168,114 @@ public class ProductView extends AbstractContentView implements ActionListener {
 		btnAdd.addActionListener(this);
 		btnAdd.setActionCommand("add");
 
-		updateNameField = new JTextField();
-		updateNameField.setBounds(93, 233, 222, 22);
-		updateNameField.setEnabled(false);
-		panel.add(updateNameField);
-		updateNameField.setColumns(10);
+		updateLabelField = new JTextField();
+		updateLabelField.setBounds(93, 233, 188, 22);
+		updateLabelField.setEnabled(false);
+		panel.add(updateLabelField);
+		updateLabelField.setColumns(10);
+		
+		JLabel lblUpdateCategory = new JLabel("Category");
+		lblUpdateCategory.setEnabled(false);
+		lblUpdateCategory.setBounds(295, 236, 56, 16);
+		panel.add(lblUpdateCategory);
 
 		JLabel lblUpdateDescription = new JLabel("Description");
-		lblUpdateDescription.setBounds(54, 271, 69, 16);
+		lblUpdateDescription.setBounds(54, 300, 69, 16);
 		lblUpdateDescription.setEnabled(false);
 		panel.add(lblUpdateDescription);
 
 		updateDescrArea = new JTextArea();
-		updateDescrArea.setBounds(130, 268, 283, 71);
+		updateDescrArea.setBounds(130, 297, 283, 42);
 		updateDescrArea.setEnabled(false);
 		panel.add(updateDescrArea);
+		
+		JLabel lblUpdateBrand = new JLabel("Brand");
+		lblUpdateBrand.setEnabled(false);
+		lblUpdateBrand.setBounds(54, 271, 39, 16);
+		panel.add(lblUpdateBrand);
+		
+		updateBrandField = new JTextField();
+		updateBrandField.setEnabled(false);
+		updateBrandField.setBounds(96, 265, 185, 22);
+		panel.add(updateBrandField);
+		updateBrandField.setColumns(10);
 
-		updateProductnameSpinner = new JSpinner();
-		updateProductnameSpinner.setBounds(130, 194, 267, 22);
-		deleteProductnameSpinner = new JSpinner();
-		deleteProductnameSpinner.setBounds(130, 423, 267, 22);
-
+		categorySpinner = new JSpinner();
+		categorySpinner.setBounds(337, 43, 153, 22);
+		updateServicecategorySpinner = new JSpinner();
+		updateServicecategorySpinner.setBounds(354, 233, 136, 22);
+		updateServicecategorySpinner.setEnabled(false);
+		
+		updateProductlabelSpinner = new JSpinner();
+		updateProductlabelSpinner.setBounds(117, 194, 267, 22);
+		deleteProductlabelSpinner = new JSpinner();
+		deleteProductlabelSpinner.setBounds(117, 423, 267, 22);
+		
+		
 		ArrayList<String> catList = new ArrayList<String>();
 		catList.add("");
 		catList.addAll(this.categories.getNames());
-		updateProductnameSpinner.setModel(new SpinnerListModel(catList));
-		deleteProductnameSpinner.setModel(new SpinnerListModel(catList));
-		updateProductnameSpinner.addChangeListener(new ChangeListener() {
+		categorySpinner.setModel(new SpinnerListModel(catList));
+		updateServicecategorySpinner.setModel(new SpinnerListModel(catList));
+		categorySpinner.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
-				String nameWrite = (String) updateProductnameSpinner.getValue();
+				String nameWrite = (String) categorySpinner.getValue();
+				selectedCategoryForCreation = categories.getCategoryWithName(nameWrite);
+			}
+		});
+		updateServicecategorySpinner.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				String nameWrite = (String) updateServicecategorySpinner.getValue();
 				selectedCategory = categories.getCategoryWithName(nameWrite);
-				if(selectedCategory == null) {
-					updateNameField.setText("");
+			}
+		});
+		panel.add(categorySpinner);
+		panel.add(updateServicecategorySpinner);
+
+		ArrayList<String> sList = new ArrayList<String>();
+		sList.add("");
+		sList.addAll(this.products.getLabels());
+		updateProductlabelSpinner.setModel(new SpinnerListModel(sList));
+		deleteProductlabelSpinner.setModel(new SpinnerListModel(sList));
+		updateProductlabelSpinner.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				String labelWrite = (String) updateProductlabelSpinner.getValue();
+				selectedProduct = products.getProductWithLabel(labelWrite);
+				if(selectedProduct == null) {
+					updateLabelField.setText("");
 					updateDescrArea.setText("");
-					lblUpdateName.setEnabled(false);
-					updateNameField.setEnabled(false);
+					lblUpdateLabel.setEnabled(false);
+					updateLabelField.setEnabled(false);
 					lblUpdateDescription.setEnabled(false);
 					updateDescrArea.setEnabled(false);
 					btnUpdate.setEnabled(false);
+					lblUpdateCategory.setEnabled(false);
+					updateServicecategorySpinner.setEnabled(false);
+					lblUpdateBrand.setEnabled(false);
+					updateBrandField.setEnabled(false);
 				}
 				else {
-					lblUpdateName.setEnabled(true);
-					updateNameField.setEnabled(true);
+					lblUpdateLabel.setEnabled(true);
+					updateLabelField.setEnabled(true);
 					lblUpdateDescription.setEnabled(true);
 					updateDescrArea.setEnabled(true);
 					btnUpdate.setEnabled(true);
-					updateNameField.setText(selectedCategory.getName());
-					updateDescrArea.setText(selectedCategory.getDescription());
+					lblUpdateCategory.setEnabled(true);
+					updateServicecategorySpinner.setEnabled(true);
+					lblUpdateBrand.setEnabled(false);
+					updateBrandField.setEnabled(false);
+					updateLabelField.setText(selectedProduct.getLabel());
+					updateDescrArea.setText(selectedProduct.getDescription());
+					updateServicecategorySpinner.setValue(selectedProduct.getCategory());
+					updateBrandField.setText(selectedProduct.getBrandName());
 				}
 			}
 		});
-		deleteProductnameSpinner.addChangeListener(new ChangeListener() {
+		deleteProductlabelSpinner.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
-				String nameWrite = (String) deleteProductnameSpinner.getValue();
-				selectedCategoryToDelete = categories.getCategoryWithName(nameWrite);
-				if(selectedCategoryToDelete == null) {
+				String labelWrite = (String) deleteProductlabelSpinner.getValue();
+				selectedProductToDelete = products.getProductWithLabel(labelWrite);
+				if(selectedProductToDelete == null) {
 					btnDelete.setEnabled(false);
 				}
 				else {
@@ -207,44 +283,93 @@ public class ProductView extends AbstractContentView implements ActionListener {
 				}
 			}
 		});
-		panel.add(updateProductnameSpinner);
-		panel.add(deleteProductnameSpinner);
+		panel.add(updateProductlabelSpinner);
+		panel.add(deleteProductlabelSpinner);
+		
+		JLabel lblBrand = new JLabel("Brand");
+		lblBrand.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblBrand.setBounds(20, 76, 47, 16);
+		panel.add(lblBrand);
+		
+		newBrandField = new JTextField();
+		newBrandField.setBounds(70, 74, 183, 22);
+		panel.add(newBrandField);
+		newBrandField.setColumns(10);
+		
+		
+		
 	}
 
 	/**
-	 * Get the name of the category to create, write in the text field from the panel INSERT
+	 * Get the label of the product to create, write in the text field from the panel INSERT
 	 * @return the text write in the text field from the panel INSERT
 	 */
-	public String getNewNameText()
+	public String getNewLabelText()
 	{
-		return this.newNameField.getText();
+		return this.newLabelField.getText();
 	}
 
 	/**
-	 * Get the description of the category to create, write in the text field from the panel INSERT
+	 * Get the description of the product to create, write in the text field from the panel INSERT
 	 * @return the text write in the text field from the panel INSERT
 	 */
 	public String getNewDescrText()
 	{
 		return this.newDescrArea.getText();
 	}
-
+	
 	/**
-	 * Get the name of the category to update, write in the text field from the panel UPDATE
-	 * @return the text write in the text field from the panel UPDATE
+	 * Get the category of the product to create
+	 * @return the category chosen
 	 */
-	public String getUpdateNameText()
+	public Category getNewCategory()
 	{
-		return this.updateNameField.getText();
+		return this.selectedCategoryForCreation;
+	}
+	
+	/**
+	 * Get the brand of the product to create, write in the text field from the panel INSERT
+	 * @return the text write in the text field from the panel INSERT
+	 */
+	public String getNewBrandText()
+	{
+		return this.newBrandField.getText();
 	}
 
 	/**
-	 * Get the description of the category to update, write in the text field from the panel UPDATE
+	 * Get the label of the product to update, write in the text field from the panel UPDATE
+	 * @return the text write in the text field from the panel UPDATE
+	 */
+	public String getUpdateLabelText()
+	{
+		return this.updateLabelField.getText();
+	}
+
+	/**
+	 * Get the description of the product to update, write in the text field from the panel UPDATE
 	 * @return the text write in the text field from the panel UPDATE
 	 */
 	public String getUpdateDescrText()
 	{
 		return this.updateDescrArea.getText();
+	}
+	
+	/**
+	 * Get the category of the product to update
+	 * @return the category chosen
+	 */
+	public Category getUpdateCategory()
+	{
+		return this.selectedCategory;
+	}
+	
+	/**
+	 * Get the brand of the product to update, write in the text field from the panel UPDATE
+	 * @return the text write in the text field from the panel UPDATE
+	 */
+	public String getUpdateBrandText()
+	{
+		return this.updateBrandField.getText();
 	}
 
 	/**
@@ -255,62 +380,68 @@ public class ProductView extends AbstractContentView implements ActionListener {
 		String cmd = arg0.getActionCommand();
 		if(cmd.equals("add"))
 		{
-			String newName = getNewNameText();
+			String newLabel = getNewLabelText();
 			String newDescr = getNewDescrText();
-			if(newName.equals("") || newDescr.equals("")) { // si tous les champs ne sont pas renseignes
+			String newBrand = getNewBrandText();
+			Category newCat = getNewCategory();
+			if(newLabel.equals("") || newDescr.equals("") || newCat.equals(null)) { // si tous les champs ne sont pas renseignes
 				String message = "All fields must be filled in.";
 				JOptionPane.showMessageDialog(null, message, "Missing fields", JOptionPane.ERROR_MESSAGE);
 			}
 			else {
-				CategoryReturnState returnState = this.facade.create(newName, newDescr);
+				ProductReturnState returnState = this.prFacade.create(newLabel, newDescr, newBrand, newCat);
 				if(returnState.isRight()) {
-					String message = "Category " + newName + " created.";
+					String message = "Product " + newLabel + " created.";
 					JOptionPane.showMessageDialog(null, message, "Creation completed", JOptionPane.INFORMATION_MESSAGE);
 					this.getMainView().setContentView(new ProductView());
 				}
 				else {
-					String message = "Error at the creation of the new category. Maybe a category with the same name is already existent.";
+					String message = "Error at the creation of the new product. Maybe a product with the same name is already existent.";
 					JOptionPane.showMessageDialog(null, message, "Creation failed", JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		}
 		else if(cmd.equals("update")) {
-			String updName = getUpdateNameText();
+			String updLabel = getUpdateLabelText();
 			String updDescr = getUpdateDescrText();
-			if(updName.equals("") || updDescr.equals("")) { // si tous les champs ne sont pas renseignes
+			String updBrand = getNewBrandText();
+			Category updCat = getUpdateCategory();
+			if(updLabel.equals("") || updDescr.equals("") ||updCat.equals(null)) { // si tous les champs ne sont pas renseignes
 				String message = "All fields must be filled in.";
 				JOptionPane.showMessageDialog(null, message, "Missing fields", JOptionPane.ERROR_MESSAGE);
 			}
 			else {
-				if(this.selectedCategory != null) {
-					String oldName = this.selectedCategory.getName();
-					this.selectedCategory.setName(updName);
-					this.selectedCategory.setDescription(updDescr);
-					CategoryReturnState returnState = this.facade.save(this.selectedCategory);
+				if(this.selectedProduct != null) {
+					String oldLabel = this.selectedProduct.getLabel();
+					this.selectedProduct.setLabel(updLabel);
+					this.selectedProduct.setDescription(updDescr);
+					this.selectedProduct.setBrandName(updBrand);
+					this.selectedProduct.setCategory(updCat);
+					ProductReturnState returnState = this.prFacade.save(this.selectedProduct);
 					if(returnState.isRight()) {
-						String message = "Category " + oldName + " updated to " + updName + ".";
+						String message = "Service " + oldLabel + " updated to " + updLabel + ".";
 						JOptionPane.showMessageDialog(null, message, "Update completed", JOptionPane.INFORMATION_MESSAGE);
 						this.getMainView().setContentView(new ProductView());
 					}
 					else {
-						String message = "Error at the update of the category. Maybe a category with the same name is already existent.";
+						String message = "Error at the update of the service. Maybe a service with the same name is already existent.";
 						JOptionPane.showMessageDialog(null, message, "Update failed", JOptionPane.ERROR_MESSAGE);
 					}
 				}
 			}
 		}
 		else if(cmd.equals("delete")) {
-			if(this.selectedCategoryToDelete != null) {
-				String oldName = this.selectedCategoryToDelete.getName();
-				boolean delete = this.facade.delete(this.selectedCategoryToDelete);
+			if(this.selectedProductToDelete != null) {
+				String oldLabel = this.selectedProductToDelete.getLabel();
+				boolean delete = this.prFacade.delete(this.selectedProductToDelete);
 				if(delete) {
-					String message = "Category " + oldName + " is deleted.";
-					JOptionPane.showMessageDialog(null, message, "Update completed", JOptionPane.INFORMATION_MESSAGE);
+					String message = "Service " + oldLabel + " is deleted.";
+					JOptionPane.showMessageDialog(null, message, "Deletion completed", JOptionPane.INFORMATION_MESSAGE);
 					this.getMainView().setContentView(new ProductView());
 				}
 				else {
-					String message = "Error at the delete of the category.";
-					JOptionPane.showMessageDialog(null, message, "Update failed", JOptionPane.ERROR_MESSAGE);
+					String message = "Error at the deletion of the service.";
+					JOptionPane.showMessageDialog(null, message, "Deletion failed", JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		}
@@ -321,6 +452,6 @@ public class ProductView extends AbstractContentView implements ActionListener {
 	 */
 	@Override
 	public String getTitle() {
-		return "Categories";
+		return "Products";
 	}
 }
