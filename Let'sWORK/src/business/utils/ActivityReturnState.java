1f38750b9d4ed4e4dec0e4c34b1b02447518d9fc
Coline <Coline@162.38.33.169>
2016-03-31 15:46:17 +0200

    JavaDoc : Activity

--- Let'sWORK/src/business/utils/ActivityReturnState.java
+++ Let'sWORK/src/business/utils/ActivityReturnState.java
@@ -14,7 +14,10 @@ public class ActivityReturnState {
 	private String categoryIdState = null;
 	private String goalIdState = null;
 	
-	// 
+	/**
+	 * Function which display if it is good or not
+	 * @return if the states are not null
+	 */
 	public boolean isRight(){
 		//No error message on name and description
 		return (nameState == null 
@@ -27,63 +30,122 @@ public class ActivityReturnState {
 	}
 
 	// GET & SET : NAME
+	/**
+	 * Get the name state of an activity and check all its rights
+	 * @return the nameState of an activity
+	 */
 	public String getNameState() {
 		return nameState;
 	}
 
+	/**
+	 * Change the name state of an activity
+	 * @param nameState : the new name state of an activity
+	 */
 	public void setNameState(String nameState){
 		this.nameState = nameState ;
 	}
 	
 	// GET & SET : DATE
+	/**
+	 * Get the date state of an activity and check all its rights
+	 * @return the dateState of an activity
+	 */
 	public String getDateState() {
 		return dateState;
 	}
+	
+	/**
+	 * Change the date state of an activity
+	 * @param dateState : the new date state of an activity
+	 */
 	public void setDateState(String dateState) {
 		this.dateState = dateState;
 	}
 
 	// GET & SET : POSITION
+	/**
+	 * Get the position state of an activity and check all its rights
+	 * @return the positionState of an activity
+	 */
 	public String getPositionState() {
 		return positionState;
 	}
 
+	/**
+	 * Change the position state of an activity
+	 * @param positionState : the new position state of an activity
+	 */
 	public void setPositionState(String positionState) {
 		this.positionState = positionState;
 	}
 	
 	// GET & SET : ISPUBLIC
+	/**
+	 * Get the visibility state of an activity and check all its rights
+	 * @return the visibilityState of an activity
+	 */
 	public String getIsPublicState() {
 		return isPublicState;
 	}
 
+	/**
+	 * Change the visibility state of an activity
+	 * @param isPublicState : the new visibility state of an activity
+	 */
 	public void setIsPublicState(String isPublicState) {
 		this.isPublicState = isPublicState;
 	}
 	
 	// GET & SET : DIARYID
+	/**
+	 * Get the diary state of an activity and check all its rights
+	 * @return the diaryState of an activity
+	 */
 	public String getDiaryState() {
 		return diaryIdState;
 	}
 
+	/**
+	 * Change the diary state of an activity
+	 * @param diaryIdState : the new diary state of an activity
+	 */
 	public void setDiaryState(String diaryIdState) {
 		this.diaryIdState = diaryIdState;
 	}
 	
 	// GET & SET : CATEGORYID
+	/**
+	 * Get the category state of an activity and check all its rights
+	 * @return the categoryState of an activity
+	 * @return
+	 */
 	public String getCategoryState() {
 		return categoryIdState;
 	}
 
+	/**
+	 * Change the category state of an activity
+	 * @param categoryIdState : the new category state of an activity
+	 */
 	public void setCategoryState(String categoryIdState) {
 		this.categoryIdState = categoryIdState;
 	}
 	
 	// GET & SET : GOALID
+	/**
+	 * Get the goal state of an activity and check all its rights
+	 * @return the goalState of an activity
+	 * @return
+	 */
 	public String getGoalState() {
 		return goalIdState;
 	}
 
+	/**
+	 * Change the goal state of an activity
+	 * @param goalIdState : the new goal state of an activity
+	 */
 	public void setGoalState(String goalIdState) {
 		this.goalIdState = goalIdState;
 	}
--- Let'sWORK/src/persistence/Activity.java
+++ Let'sWORK/src/persistence/Activity.java
@@ -32,12 +32,25 @@ public abstract class Activity {
 	
 	// ABSTRACT METHODS
 	
+	/**
+	 * Create a new activity
+	 * @throws SaveException
+	 */
 	public abstract void save() throws SaveException;
+	
+	/**
+	 * Delete the activity selected
+	 * @throws DeleteException
+	 */
 	public abstract void delete() throws DeleteException;
 	
 	// GETTER AND SETTER
 	
 	// Accesseurs & Mutateurs : NAME
+	/**
+	 * Get the name of an activity
+	 * @return the name of an activity
+	 */
 	public String getName() {
 		return this.name;
 	}
@@ -47,55 +60,103 @@ public abstract class Activity {
 	}
 	
 	// Accesseurs & Mutateurs : DATE
+	/**
+	 * Get the date of an activity
+	 * @return the date of an activity
+	 */
 	public Date getDate() {
 		return this.date;
 	}
 	
+	/**
+	 * Change the date of an activity
+	 * @param dateDetail : the new date of the activity
+	 */
 	public void setDate(Date dateDetail) {
 		this.date = dateDetail;
 	}
 	
 	// Accesseurs & Mutateurs : POSITION
+	/**
+	 * Get the time position of an activity
+	 * @return the time position of an activity
+	 */
 	public TimePosition getPosition() {
 		return this.position;
 	}
 		
+	/**
+	 * Change the position time of an activity
+	 * @param position : the new position time of the activity
+	 */
 	public void setPosition(TimePosition position) {
 		this.position = position;
 	}
 		
 	// Accesseurs & Mutateurs : ISPUBLIC
+	/**
+	 * Get if the activity is public
+	 * @return true if the activity is public, false otherwise
+	 */
 	public boolean getIsPublic() {
 		return this.isPublic;
 	}
 	
+	/**
+	 * Change the visibility of an activity
+	 * @param estPublic : the new visibility of the activity
+	 */
 	public void setIsPublic(boolean estPublic) {
 		this.isPublic = estPublic;
 	}
 	
 	// Accesseurs & Mutateurs : JOURNALID
+	/**
+	 * Get the diary where the activity is placed
+	 * @return the name of an activity
+	 */
 	public Diary getDiary() {
 		return this.diary;
 	}
 			
+	/**
+	 * Change the journal id of an activity
+	 * @param journalId : the new journal id of the activity
+	 */
 	public void setDiary(Diary journalId) {
 		this.diary = journalId;
 	}
 	
 	// Accesseurs & Mutateurs : CATEGORYID
+	/**
+	 * Get the category of an activity
+	 * @return the category of an activity
+	 */
 	public Category getCategory() {
 		return this.category;
 	}
 	
+	/**
+	 * Change the category of an activity
+	 * @param cat : the new category of the activity
+	 */
 	public void setCategory(Category cat) {
 		this.category = cat;
 	}
 	
 	// Accesseurs & Mutateurs : GOALID
+	/**
+	 * Get the goal of the activity
+	 * @return the goal of the activity
+	 */
 	public Goal getGoal() {
 		return this.goal;
 	}
 				
+	/**
+	 * Change the goal of an activity
+	 * @param but : the new goal of the activity
+	 */
 	public void setGoal(Goal but) {
 		this.goal = but;
 	}
